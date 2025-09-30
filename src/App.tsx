a5b5f37ca069c3cf7d6f0de835e2d3a5b17bfae2
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-09-30 11:43:16 +0200

    Created the hooks.ts file to make typed versions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import './App.css'
+import './App.css';
+import { WordSearch } from './components/WordSearch';
+import { DefinitionDisplay } from './components/DefinitionDisplay';
 
 function App() {
-  
   return (
     <>
+      <h1>Dictionary</h1>
+      <WordSearch />
+      <hr />
+      <DefinitionDisplay />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/WordSearch.tsx
@@ -0,0 +1,28 @@
+// This component will have an input field and a button. On click, it will dispatch the fetchWordDefinition thunk.
+
+import React, { useState } from 'react';
+import { useAppDispatch } from '../../hooks';
+import { fetchWordDefinition } from '../features/dictionary/dictionarySlice';
+
+export const WordSearch: React.FC = () => {
+  const [word, setWord] = useState('');
+  const dispatch = useAppDispatch();
+
+  const handleSearch = () => {
+    if (word.trim()) {
+      dispatch(fetchWordDefinition(word.trim()));
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={word}
+        onChange={(e) => setWord(e.target.value)}
+        placeholder="Enter a word"
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
